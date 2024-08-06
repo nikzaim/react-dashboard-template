9f0d3bdec01a5e1c56d57b63edc1b2077810f488
nikzaim <nikzaim@users.noreply.github.com>
2024-08-06 23:28:01 +0800

    add tanstack router, shadcn, tailwind and biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.ts
+import path from "path";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [TanStackRouterVite(), viteReact()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
