7bb3709bad928710ef01311e1e9a5d2497aaa08e
nikzaim <nikzaim@users.noreply.github.com>
2024-08-08 00:08:07 +0800

    add scroll area

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: () => <div>Hello /!</div>,
+  component: Index,
 });
+
+function Index() {
+  return (
+    <>
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">Index</h1>
+      </div>
+      <div className="flex h-72 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1">
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1>Index</h1>
+        </div>
+      </div>
+    </>
+  );
+}
